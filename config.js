3a2b378ef9603115e85767b995ae0df44c2c9feb
Vrukshav <84898245+Vrukshav@users.noreply.github.com>
2022-06-15 18:02:41 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+import firebase from "firebase";
+require("@firebase/firestore");
+
+//Paste your firebaseConfig here
+const firebaseConfig = {
+    apiKey: "AIzaSyAT_1hSbfFcOJ-cHuhgEnpXPrVMRE4oVDQ",
+    authDomain: "e-ride-d04fc.firebaseapp.com",
+    projectId: "e-ride-d04fc",
+    storageBucket: "e-ride-d04fc.appspot.com",
+    messagingSenderId: "390753424039",
+    appId: "1:390753424039:web:278c6656e73527bbbc4182"
+  };
+  
+//
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.firestore();
